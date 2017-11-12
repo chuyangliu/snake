6e74aced92bcad69894cc8b063ed2b72b75812d2
Chuyang Liu <chuyang.s.liu@gmail.com>
2017-11-12 14:43:19 +0800

    Change default map size to 10*10

--- src/main.cpp
+++ src/main.cpp
@@ -25,8 +25,8 @@ int main() {
     game->setRunTest(false);
 
     // Set map's size(including boundaries). Default is 10*10. Minimum is 5*5.
-    game->setMapRow(16);
-    game->setMapCol(16);
+    game->setMapRow(10);
+    game->setMapCol(10);
 
     return game->run();
 }
