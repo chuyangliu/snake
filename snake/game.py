bd1bc7658972553928f777e81d5fa8d0d2bdf702
Chuyang Liu <chuyang.s.liu@gmail.com>
2017-12-09 16:57:19 +0800

    Finish game elements drawing

--- snake/game.py
+++ snake/game.py
@@ -10,8 +10,26 @@ class GameConf:
 
     def __init__(self):
         """Initialize a default configuration."""
-        self.width = 12
-        self.height = 12
+
+        # Size
+        self.map_rows = 20
+        self.map_cols = 20
+        self.window_width = 300
+        self.window_height = 300
+        self.grid_pad_ratio = 0.25
+
+        # Switch
+        self.show_grid_line = False
+
+        # Color
+        self.color_grid_line = '#424242'
+        self.color_bg = '#000000'
+        self.color_wall = '#F5F5F5'
+        self.color_food = '#F5F5F5'
+        self.color_head = '#F5F5F5'
+        self.color_body = '#F5F5F5'
+
+        # Initial snake
         self.init_direc = Direc.RIGHT
         self.init_bodies = [Pos(1, 3), Pos(1, 2), Pos(1, 1)]
         self.init_types = [PointType.HEAD_R, PointType.BODY_HOR, PointType.BODY_HOR]
@@ -26,10 +44,11 @@ class Game:
             conf (snake.game.GameConf): Game configuration.
 
         """
-        self.__map = Map(conf.width, conf.height)
+        self.__conf = conf
+        self.__map = Map(conf.map_rows + 2, conf.map_cols + 2)
         self.__snake = Snake(self.__map, conf.init_direc,
                              conf.init_bodies, conf.init_types)
-        self.__window = GameWindow(self.__map)
+        self.__window = GameWindow(self.__map, conf)
 
     def run(self):
         self.__window.after(100, self.__game_loop)
---
+++ tests/test_gui.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# pylint: disable=C0111
+
+"""GUI Tests."""
+
+import pytest
+from snake.base import Pos, PointType, Map
+from snake.game import GameConf
+from snake.gui import GameWindow
+
+
+@pytest.mark.skip()
+def test_gui():
+    game_conf = GameConf()
+    game_conf.map_rows = 15
+    game_conf.map_cols = 15
+    game_conf.show_grid_line = True
+
+    game_map = Map(game_conf.map_rows + 2, game_conf.map_cols + 2)
+    contents = (
+        # Walls
+        (Pos(1, 6), PointType.WALL),
+        (Pos(1, 7), PointType.WALL),
+        (Pos(1, 8), PointType.WALL),
+        (Pos(1, 9), PointType.WALL),
+        (Pos(1, 10), PointType.WALL),
+        (Pos(15, 6), PointType.WALL),
+        (Pos(15, 7), PointType.WALL),
+        (Pos(15, 8), PointType.WALL),
+        (Pos(15, 9), PointType.WALL),
+        (Pos(15, 10), PointType.WALL),
+        (Pos(6, 1), PointType.WALL),
+        (Pos(7, 1), PointType.WALL),
+        (Pos(8, 1), PointType.WALL),
+        (Pos(9, 1), PointType.WALL),
+        (Pos(10, 1), PointType.WALL),
+        (Pos(6, 15), PointType.WALL),
+        (Pos(7, 15), PointType.WALL),
+        (Pos(8, 15), PointType.WALL),
+        (Pos(9, 15), PointType.WALL),
+        (Pos(10, 15), PointType.WALL),
+        # Food
+        (Pos(4, 6), PointType.FOOD),
+        (Pos(4, 10), PointType.FOOD),
+        (Pos(6, 4), PointType.FOOD),
+        (Pos(10, 4), PointType.FOOD),
+        (Pos(6, 12), PointType.FOOD),
+        (Pos(10, 12), PointType.FOOD),
+        (Pos(12, 6), PointType.FOOD),
+        (Pos(12, 10), PointType.FOOD),
+        # Top-left
+        (Pos(2, 2), PointType.BODY_VER),
+        (Pos(3, 2), PointType.BODY_VER),
+        (Pos(4, 2), PointType.BODY_UR),
+        (Pos(4, 3), PointType.BODY_LU),
+        (Pos(3, 3), PointType.BODY_VER),
+        (Pos(2, 3), PointType.BODY_RD),
+        (Pos(2, 4), PointType.BODY_DL),
+        (Pos(2, 4), PointType.BODY_DL),
+        (Pos(3, 4), PointType.BODY_VER),
+        (Pos(4, 4), PointType.HEAD_D),
+        # Top-right
+        (Pos(2, 14), PointType.BODY_VER),
+        (Pos(3, 14), PointType.BODY_VER),
+        (Pos(4, 14), PointType.BODY_LU),
+        (Pos(4, 13), PointType.BODY_UR),
+        (Pos(3, 13), PointType.BODY_VER),
+        (Pos(2, 13), PointType.BODY_DL),
+        (Pos(2, 12), PointType.BODY_RD),
+        (Pos(3, 12), PointType.BODY_VER),
+        (Pos(4, 12), PointType.HEAD_D),
+        # Bottom-left
+        (Pos(14, 2), PointType.BODY_VER),
+        (Pos(13, 2), PointType.BODY_VER),
+        (Pos(12, 2), PointType.BODY_RD),
+        (Pos(12, 3), PointType.BODY_DL),
+        (Pos(13, 3), PointType.BODY_VER),
+        (Pos(14, 3), PointType.BODY_UR),
+        (Pos(14, 4), PointType.BODY_LU),
+        (Pos(13, 4), PointType.BODY_VER),
+        (Pos(12, 4), PointType.HEAD_U),
+        # Bottom-right
+        (Pos(14, 14), PointType.BODY_VER),
+        (Pos(13, 14), PointType.BODY_VER),
+        (Pos(12, 14), PointType.BODY_DL),
+        (Pos(12, 13), PointType.BODY_RD),
+        (Pos(13, 13), PointType.BODY_VER),
+        (Pos(14, 13), PointType.BODY_LU),
+        (Pos(14, 12), PointType.BODY_UR),
+        (Pos(13, 12), PointType.BODY_VER),
+        (Pos(12, 12), PointType.HEAD_U),
+        # Middle
+        (Pos(10, 6), PointType.HEAD_L),
+        (Pos(10, 7), PointType.BODY_HOR),
+        (Pos(10, 8), PointType.BODY_HOR),
+        (Pos(10, 9), PointType.BODY_HOR),
+        (Pos(10, 10), PointType.BODY_LU),
+        (Pos(9, 10), PointType.BODY_VER),
+        (Pos(8, 10), PointType.BODY_DL),
+        (Pos(8, 9), PointType.BODY_HOR),
+        (Pos(8, 8), PointType.BODY_HOR),
+        (Pos(8, 7), PointType.BODY_HOR),
+        (Pos(8, 6), PointType.BODY_UR),
+        (Pos(7, 6), PointType.BODY_VER),
+        (Pos(6, 6), PointType.BODY_RD),
+        (Pos(6, 7), PointType.BODY_HOR),
+        (Pos(6, 8), PointType.BODY_HOR),
+        (Pos(6, 9), PointType.BODY_HOR),
+        (Pos(6, 10), PointType.HEAD_R),
+    )
+    for content in contents:
+        game_map.point(content[0]).type = content[1]
+
+    window = GameWindow(game_map, game_conf)
+
+    def render():
+        while True:
+            window.render()
+
+    window.after(100, render)
+    window.mainloop()
