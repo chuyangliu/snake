a68d351def04cc190ac11487a4d47d2f1bcebe5c
Chuyang Liu <chuyang.s.liu@gmail.com>
2017-12-18 23:01:32 +0800

    Add log file

--- snake/game.py
+++ snake/game.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=C0103,C0111
 
+import os
+import errno
 from snake.base import Direc, Pos, PointType, Map, Snake
 from snake.gui import GameWindow
 
@@ -71,7 +73,7 @@ class Game:
         self.__snake = Snake(self.__map, conf.init_direc,
                              conf.init_bodies, conf.init_types)
         self.__pause = False
-        self.__window = GameWindow(conf, self.__map, self.__snake, (
+        self.__window = GameWindow(conf, self.__map, self.__snake, self.__on_exit, (
             ('<w>', lambda e: self.__update_direc(Direc.UP)),
             ('<a>', lambda e: self.__update_direc(Direc.LEFT)),
             ('<s>', lambda e: self.__update_direc(Direc.DOWN)),
@@ -79,6 +81,7 @@ class Game:
             ('<r>', lambda e: self.__reset()),
             ('<space>', lambda e: self.__toggle_pause())
         ))
+        self.__init_log_file()
 
     def run(self):
         self.__window.show(self.__game_main)
@@ -86,8 +89,9 @@ class Game:
     def __game_main(self):
         if not self.__map.has_food():
             self.__map.create_rand_food()
-        if not self.__pause:
+        if not self.__pause and not self.__snake.dead:
             self.__snake.move()
+            self.__write_logs()
 
     def __update_direc(self, new_direc):
         if Direc.opposite(new_direc) != self.__snake.direc:
@@ -102,3 +106,41 @@ class Game:
         self.__pause = True
         self.__snake.reset()
         self.__pause = False
+
+    def __on_exit(self):
+        if self.__log_file:
+            self.__log_file.close()
+
+    def __init_log_file(self):
+        try:
+            os.makedirs("logs")
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+        try:
+            self.__log_file = None
+            self.__log_file = open('logs/snake.log', 'w')
+        except FileNotFoundError:
+            if self.__log_file:
+                self.__log_file.close()
+
+    def __write_logs(self):
+        self.__log_file.write("Step %d:\n" % self.__snake.steps)
+        # Map content
+        for i in range(self.__map.num_rows):
+            for j in range(self.__map.num_cols):
+                t = self.__map.point(Pos(i, j)).type
+                if t == PointType.EMPTY:
+                    self.__log_file.write("  ")
+                elif t == PointType.WALL:
+                    self.__log_file.write("# ")
+                elif t == PointType.FOOD:
+                    self.__log_file.write("F ")
+                elif t == PointType.HEAD_L or t == PointType.HEAD_U or \
+                    t == PointType.HEAD_R or t == PointType.HEAD_D:
+                    self.__log_file.write("H ")
+                else:
+                    self.__log_file.write("B ")
+            self.__log_file.write("\n")
+        self.__log_file.write("Last direc: %s\n" % str(self.__snake.direc))
+        self.__log_file.write("\n")
