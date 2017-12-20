606d2467e025bc6633bebc904abfacf10f5cf809
Chuyang Liu <chuyang.liu@outlook.com>
2017-12-20 16:17:23 +0800

    Add BaseSolver

---
+++ snake/solver/base.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# pylint: disable=C0111
+
+
+class BaseSolver:
+    """Super class of all the solvers."""
+
+    def __init__(self, snake):
+        self.__snake = snake
+        self.__map = snake.map
+
+    @property
+    def map(self):
+        return self.__map
+
+    @property
+    def snake(self):
+        return self.__snake
+
+    @snake.setter
+    def snake(self, val):
+        self.__snake = val
+        self.__map = val.map
+
+    def next_direc(self):
+        """Generate the next direction of the snake."""
+        return NotImplemented
