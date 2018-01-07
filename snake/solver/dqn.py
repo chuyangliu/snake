fedc9a40e0d3639ad6a269892f73a70d6aeeb524
Chuyang Liu <chuyang.liu@outlook.com>
2018-01-07 15:55:08 +0800

    Update Snake: do not move opposite & Update DQNSolver: cal q target in tf graph

--- snake/solver/dqn.py
+++ snake/solver/dqn.py
@@ -31,7 +31,7 @@ class DQNSolver(BaseSolver):
         self.__gamma = 0.99
 
         # Epsilon-greedy
-        self.__epsilon_max = 1
+        self.__epsilon_max = 0.9
         self.__epsilon_min = 0.1
         self.__epsilon_decreasement = 1e-6
         self.__epsilon = self.__epsilon_max
@@ -42,16 +42,16 @@ class DQNSolver(BaseSolver):
         # Frequency (learn steps) for replacement of target net parameters
         self.__replace_target_iter = 10000
 
-        # Number of actions and features
-        self.__actions = [Direc.LEFT, Direc.UP, Direc.RIGHT, Direc.DOWN]
-        self.__num_actions = len(self.__actions)
+        # Actions and features
+        self.__snake_actions = [Direc.LEFT, Direc.UP, Direc.RIGHT, Direc.DOWN]
+        self.__num_actions = len(self.__snake_actions)
         self.__num_features = snake.map.capacity
 
         # Reward values
         self.__reward_dict = {
             "EMPTY": -0.001,
             "DEAD": -0.1,
-            "FOOD": 0.1,
+            "FOOD": 0.5,
             "FULL": 1.0
         }
 
@@ -77,11 +77,11 @@ class DQNSolver(BaseSolver):
         # Write graph logs
         log_dir = "./logs"
         tf.summary.FileWriter(log_dir, self.__sess.graph)
-        _log("logdir: %s" % log_dir)
+        _log("--logdir=%s" % log_dir)
 
     def __build_net(self):
 
-        def __build_layers(x, scope, w_init_, b_init_):
+        def __build_layers(x, scope, name, w_init_, b_init_):
             with tf.variable_scope(scope):
                 x_2d = tf.reshape(x, [-1, 10, 10, 1])
                 conv1 = tf.layers.conv2d(inputs=x_2d,
@@ -122,8 +122,8 @@ class DQNSolver(BaseSolver):
                                     units=self.__num_actions,
                                     kernel_initializer=w_init_,
                                     bias_initializer=b_init_,
-                                    name="q")
-                return q  # Shape: (None, num_actions)
+                                    name=name)
+                return q
 
         # Input tensor for eval net
         self.__state_eval = tf.placeholder(
@@ -134,31 +134,46 @@ class DQNSolver(BaseSolver):
             tf.float32, [None, self.__num_features], name="state_target")
 
         # Input tensor for actions taken by agent
-        self.__actions_taken = tf.placeholder(
-            tf.int32, [None, ], name="actions_taken")
+        self.__action = tf.placeholder(
+            tf.int32, [None, ], name="action")
 
-        # Input tensor for calculated q target
-        self.__q_target = tf.placeholder(
-            tf.float32, [None, ], name="q_target")
+        # Input tensor for rewards received by agent
+        self.__reward = tf.placeholder(
+            tf.float32, [None, ], name="reward")
 
-        scope_eval, scope_target = "eval_net", "target_net"
+        # Input tensor for calculated eval net output for next state
+        self.__q_eval_all_nxt = tf.placeholder(
+            tf.float32, [None, self.__num_actions], name="q_eval_all_nxt")
+
         w_init = tf.truncated_normal_initializer(mean=0, stddev=0.1)
         b_init = tf.constant_initializer(0.1)
 
+        scope_eval, scope_target = "eval_net", "target_net"
+
         # Eval net output / Shape: (None, num_actions)
-        self.__q_eval_all = __build_layers(self.__state_eval, scope_eval, w_init, b_init)
+        self.__q_eval_all = __build_layers(
+            self.__state_eval, scope_eval, "q_eval_all", w_init, b_init)
 
         with tf.variable_scope("q_eval"):
             indices = tf.range(tf.shape(self.__state_eval)[0], dtype=tf.int32)
-            action_indices = tf.stack([indices, self.__actions_taken], axis=1)
+            action_indices = tf.stack([indices, self.__action], axis=1)
             # Shape: (None, )
-            self.__q_eval = tf.gather_nd(self.__q_eval_all, action_indices)
+            q_eval = tf.gather_nd(self.__q_eval_all, action_indices)
 
         # Target net output / Shape: (None, num_actions)
-        self.__q_nxt_all = __build_layers(self.__state_target, scope_target, w_init, b_init)
+        q_nxt_all = __build_layers(
+            self.__state_target, scope_target, "q_nxt_all", w_init, b_init)
+
+        with tf.variable_scope("q_target"):
+            indices = tf.range(tf.shape(self.__state_target)[0], dtype=tf.int32)
+            max_actions = tf.argmax(self.__q_eval_all_nxt, axis=1, output_type=tf.int32)
+            action_indices = tf.stack([indices, max_actions], axis=1)
+            # Shape: (None, )
+            q_nxt = tf.gather_nd(q_nxt_all, action_indices, name="q_nxt")
+            q_target = self.__reward + self.__gamma * q_nxt
 
         with tf.variable_scope("loss"):
-            self.__loss = tf.reduce_mean(tf.squared_difference(self.__q_target, self.__q_eval))
+            self.__loss = tf.reduce_mean(tf.squared_difference(q_eval, q_target))
 
         with tf.variable_scope("train"):
             self.__train = tf.train.RMSPropOptimizer(
@@ -170,7 +185,7 @@ class DQNSolver(BaseSolver):
         return eval_params, target_params
 
     def next_direc(self):
-        return self.__actions[self.__choose_action()]
+        return self.__snake_actions[self.__choose_action()]
 
     def train(self):
         action = self.__choose_action()
@@ -183,36 +198,24 @@ class DQNSolver(BaseSolver):
             self.__epsilon -= self.__epsilon_decreasement
 
     def __choose_action(self):
-        oppo_direc = Direc.opposite(self.snake.direc)
-
         if np.random.uniform() < self.__epsilon:
-            while True:
-                action_idx = np.random.randint(0, self.__num_actions)
-                if self.__actions[action_idx] != oppo_direc:
-                    break
+            action_idx = np.random.randint(0, self.__num_actions)
         else:
-            state = self.map.state()
-            state = state[np.newaxis, :]
+            state = self.map.state()[np.newaxis, :]
             q_eval_all = self.__sess.run(
                 self.__q_eval_all,
                 feed_dict={
                     self.__state_eval: state
                 }
             )
-            # Select an action with max value
-            max_val = None
-            for i, val in enumerate(q_eval_all[0]):
-                direc = self.__actions[i]
-                if direc != oppo_direc and (max_val is None or val > max_val):
-                    max_val = val
-                    action_idx = i
-
+            action_idx = np.argmax(q_eval_all, axis=1)[0]
         return action_idx
 
     def __step(self, action_idx):
-        direc = self.__actions[action_idx]
+        direc = self.__snake_actions[action_idx]
         nxt_pos = self.snake.head().adj(direc)
         nxt_type = self.map.point(nxt_pos).type
+
         if nxt_type == PointType.EMPTY:
             reward = self.__reward_dict["EMPTY"]
         elif nxt_type == PointType.FOOD:
@@ -221,6 +224,7 @@ class DQNSolver(BaseSolver):
             reward = self.__reward_dict["DEAD"]
 
         self.snake.move(direc)
+
         if self.map.is_full():
             reward = self.__reward_dict["FULL"]
 
@@ -244,34 +248,31 @@ class DQNSolver(BaseSolver):
         sample_indices = np.random.choice(
             num_available, size=min(num_available, self.__mem_batch), replace=False)
         batch = self.__mem[sample_indices, :]
-        batch_indices = np.arange(batch.shape[0], dtype=np.int32)
         batch_state_cur = batch[:, :self.__num_features]
         batch_action = batch[:, self.__num_features].astype(np.int32)
         batch_reward = batch[:, self.__num_features + 1]
         batch_state_nxt = batch[:, -self.__num_features:]
 
-        # Compute q target
-        q_nxt_all, q_eval_all_nxt = self.__sess.run(
-            [self.__q_nxt_all, self.__q_eval_all],
+        # Compute eval net output for next state (to compute q target)
+        q_eval_all_nxt = self.__sess.run(
+            self.__q_eval_all,
             feed_dict={
                 self.__state_eval: batch_state_nxt,
-                self.__state_target: batch_state_nxt
             }
         )
-        max_actions = np.argmax(q_eval_all_nxt, axis=1)
-        q_nxt = q_nxt_all[batch_indices, max_actions]
-        q_target = batch_reward + self.__gamma * q_nxt
 
         # Learn
         _, loss = self.__sess.run(
             [self.__train, self.__loss],
             feed_dict={
                 self.__state_eval: batch_state_cur,
-                self.__actions_taken: batch_action,
-                self.__q_target: q_target
+                self.__state_target: batch_state_nxt,
+                self.__action: batch_action,
+                self.__reward: batch_reward,
+                self.__q_eval_all_nxt: q_eval_all_nxt
             }
         )
         log_msg += " | Loss: %f" % loss
 
-        _log(log_msg)
         self.__learn_steps += 1
+        _log(log_msg)
