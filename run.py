47633bb9a423f29caf317aac8347312b1d3ebe55
Chuyang Liu <chuyang.s.liu@gmail.com>
2018-03-17 09:00:36 +0800

    Update DQNSolver: add algorithm switches & Update run.py: pass args from cmd line

--- run.py
+++ run.py
@@ -2,20 +2,33 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=C0103,C0111
 
-from snake.game import GameMode, GameConf, Game
+import argparse
 
+from snake.game import Game, GameConf, GameMode
 
-# Available solvers
-solvers = [
-    "GreedySolver",
-    "HamiltonSolver",
-    "DQNSolver"
-]
+dict_solver = {
+    "greedy": "GreedySolver",
+    "hamilton": "HamiltonSolver",
+    "dqn": "DQNSolver",
+}
 
-# Game configurations
+dict_mode = {
+    "normal": GameMode.NORMAL,
+    "bcmk": GameMode.BENCHMARK,
+    "train_dqn": GameMode.TRAIN_DQN,
+    "train_dqn_gui": GameMode.TRAIN_DQN_GUI,
+}
+
+parser = argparse.ArgumentParser(description="Run snake game agent.")
+parser.add_argument("-s", default="hamilton", choices=dict_solver.keys(),
+                    help="name of the solver to direct the snake (default: hamilton)")
+parser.add_argument("-m", default="normal", choices=dict_mode.keys(),
+                    help="game mode (default: normal)")
+args = parser.parse_args()
+
 conf = GameConf()
-conf.mode = GameMode.TRAIN_DQN
-conf.solver_name = solvers[2]
+conf.solver_name = dict_solver[args.s]
+conf.mode = dict_mode[args.m]
+print("Solver: %s    Mode: %s" % (conf.solver_name, conf.mode))
 
-# Run the game
 Game(conf).run()
